26a1ee0b7fcebf5d506cc82269ae9dd716a5272a
austinaeden <austin.mbogo@student.moringaschool.com>
2026-09-02 09:24:24 +0300

    activities set up

---
+++ app/src/main/java/com/example/ecotrack/ui/TrackingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecotrack.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecotrack.R
+
+class TrackingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracking)
+    }
+}
